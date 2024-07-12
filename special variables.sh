4434d8ae6316f471e644afa0068a56fba1e07fe3
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-13 00:17:37 +0530

    shell

--- special variables.sh
+++ special variables.sh
@@ -1,5 +1,11 @@
 echo "get all variables in a list: $@"
 echo "get the number of variables you passes: $#"
+echo "want to know the script name: $0"
 echo "to know the current working directory: $PWD"
+echo "to know which user is running this script: $USER"
+echo "home directory of current user: $HOME"
+echo "to know the process id of the current script: $$"
+echo "to know the hostname: $HOSTNAME"
+
 
 
