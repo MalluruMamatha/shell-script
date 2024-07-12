454d999f4ccd3547fd357b2eca2d66538f1539d2
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-12 23:43:22 +0530

    shell

--- variables.sh
+++ variables.sh
@@ -1,4 +1,4 @@
-
+### printing the below content
 echo "ramesh: Hi,suresh,how are you?"
 echo "suresh: Hi,ramesh,i am good."
 echo "ramesh: How is your day?"
@@ -23,3 +23,14 @@ echo "$PERSON1: Hi,$PERSON2,how are you?"
 echo "$PERSON2: Hi,$PERSON1,i am good."
 echo "$PERSON1: How is your day?"
 echo "$PERSON2: I spend my day very well"
+
+# o/p command: sh variables.sh ramu mamatha ,$1 is ramu $2 is mamatha
+
+### declering the variables through prompt
+
+echo "please enter the username"
+read username
+echo "please enter the password"
+read password
+echo "my username is: $username , my password is: $password"
+
