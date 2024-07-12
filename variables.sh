daa1ba49fd0dd8592d514d205cb6980af7437ffa
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-12 23:34:42 +0530

    shell

--- variables.sh
+++ variables.sh
@@ -1,8 +1,15 @@
 
-
-
-
 echo "ramesh: Hi,suresh,how are you?"
 echo "suresh: Hi,ramesh,i am good."
 echo "ramesh: How is your day?"
 echo "suresh: I spend my day very well"
+
+### declering variables
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1: Hi,$PERSON2,how are you?"
+echo "$PERSON2: Hi,$PERSON1,i am good."
+echo "$PERSON1: How is your day?"
+echo "$PERSON2: I spend my day very well"
