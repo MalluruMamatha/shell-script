a78f3e63c184a56cbf34cfd4e09eb86b93ec500b
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-13 00:32:52 +0530

    shell

--- conditions.sh
+++ conditions.sh
@@ -1,6 +1,6 @@
 if [ $NUMBER -gt 10 ]
 then
-   echo "The given number is greater than ${NUMBER}"
+   echo "The given number is greater than {$NUMBER}"
 else
-   echo "The given number is less than ${NUMBER}"
+   echo "The given number is less than {$NUMBER}"
 fi
