f28e97148110b03586263d722d15713351cd4255
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-13 00:30:49 +0530

    shell

---
+++ conditions.sh
@@ -0,0 +1,6 @@
+if [$NUMBER -gt 10]
+then
+   "The given number is greater than ${NUMBER}"
+else
+   "The given number is less than ${NUMBER}"
+fi
--- special variables.sh
+++ special variables.sh
@@ -6,6 +6,6 @@ echo "to know which user is running this script: $USER"
 echo "home directory of current user: $HOME"
 echo "to know the process id of the current script: $$"
 echo "to know the hostname: $HOSTNAME"
-
+echo "to know the process ID of last background command: $!"
 
 
