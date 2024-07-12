84afee323eaee4cdb06072a4a43e17b831d6603d
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-13 00:09:57 +0530

    shell

---
+++ special variables.sh
@@ -0,0 +1,9 @@
+# echo "get all variables in a list: $@"
+# echo "
+
+
+echo "please enter the username"
+read -s username
+echo "please enter the password"
+read -s password
+echo "my username is: $username , my password is: $password"
